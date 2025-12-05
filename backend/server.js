50deda2d35d9fde888b127c4a1d4b64820d0a3b3
Wendell <wendelljesusprofissional@gmail.com>
2025-12-05 13:30:32 -0300

    teste4

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, '../frontend/pages'));
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Rotas
-const EnviarEmail = require("./EnviarEmail");
+const EnviarEmail = require("./routes/EnviarEmail");
 
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
