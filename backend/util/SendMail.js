caa6aabbfd9dc75d9b5ded653191ac0cbd3358e0
Wendell <wendelljesusprofissional@gmail.com>
2025-12-07 21:48:59 -0300

    Implementando Resend

--- backend/util/SendMail.js
+++ backend/util/SendMail.js
@@ -6,18 +6,19 @@ async function SendEmail(req, res) {
     try {
         const { username, useremail, usermessage } = req.body;
 
-        await resend.emails.send({
-            from: "Trinity Tech Group <trinitytechofc@gmail.com>",
+        const data = await resend.emails.send({
+            from: "Trinity Tech Group <onboarding@resend.dev>",
             to: useremail,
             subject: "Obrigado pelo contato!",
             html: `
                 <h3>Obrigado ${username}!</h3>
                 <p>Recebemos sua mensagem:</p>
                 <p>${usermessage}</p>
-                <p style="color:red">Entraremos em contato em até 1 dia útil.</p>
+                <p style="color:red;">Entraremos em contato em até 1 dia útil.</p>
             `
         });
 
+        console.log("EMAIL ENVIADO:", data);
         return res.redirect("/");
     } catch (error) {
         console.error("Erro ao enviar email:", error);
